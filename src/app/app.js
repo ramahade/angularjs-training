e19c904c9f1b6269b578287957fbb8643a88aeeb
Rajesh Mahadevaiyer <rajmahad@cisco.com>
2016-12-07 09:52:07 +0530

    Binding, Interpolation samples added

--- src/app/app.js
+++ src/app/app.js
@@ -49,5 +49,32 @@
                     return me.result;
                 }
             }
+        ])
+        /**
+         * Controller for Binding Samples.
+         */
+        .controller("BindingSamplesController", ["$scope", "$timeout", "$log",
+            function ($scope, $timeout, $log) {
+                var me = this;
+
+                var timeoutPromise = $timeout(populateModel, 3000);
+                $scope.$on("$destroy", cleanUp);
+
+                /**
+                 * Populate the model with a greeting
+                 */
+                function populateModel(){
+                    $log.info("populating model");
+                    me.greeting = "Hello Folks!";
+                }
+
+                /**
+                 * Cleans up any timeouts or listeners left behind
+                 */
+                function cleanUp(){
+                    $log.info("cleanup");
+                    $timeout.cancel(timeoutPromise);
+                }
+            }
         ]);
 }());
